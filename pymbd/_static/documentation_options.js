82f8fe330739c3bd35662d711a7e4adb2e5d887e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-08 12:58:25 +0000

    Deploy jhrmnn/libmbd to github.com/jhrmnn/libmbd.git:gh-pages

--- pymbd/_static/documentation_options.js
+++ pymbd/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11.0.post2.dev0+96fc666',
+    VERSION: '0.11.0.post4.dev0+2d278ea',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
